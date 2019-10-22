dc6f48af777da45349878b29f13ea90c34db946d
Eduardo Acacio <acaciosh@gmail.com>
2019-10-21 23:09:20 -0300

    resolvendo valores menores que zero

--- pre_processamento_credit_data.py
+++ pre_processamento_credit_data.py
@@ -2,9 +2,20 @@
 """
 Created on Mon Oct 21 17:00:39 2019
 
-@author: 1008220
+@author: Eduardo Acacio
+
 """
 
 import pandas as pd
 base = pd.read_csv('credit-data.csv')
 base.describe()
+base.loc[base['age'] < 0]
+
+base.drop('age', 1, inplace=True)
+
+base.drop(base[base.age < 0].index, inplace=True)
+
+base.mean()
+base['age'].mean()
+base['age'][base.age > 0].mean()
+base.loc[base.age < 0, 'age'] = 40.92
