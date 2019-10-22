c109ae5d94d3b74ba81d0c4d41401b5450f90787
Eduardo Acacio <acaciosh@gmail.com>
2019-10-21 23:33:51 -0300

    adicionando escalonamento

--- pre_processamento_credit_data.py
+++ pre_processamento_credit_data.py
@@ -19,3 +19,19 @@ base.mean()
 base['age'].mean()
 base['age'][base.age > 0].mean()
 base.loc[base.age < 0, 'age'] = 40.92
+
+pd.isnull(base['age'])
+base.loc[pd.isnull(base['age'])]
+
+previsores = base.iloc[:, 1:4].values
+classe = base.iloc[:, 4].values
+
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values='NaN', strategy='mean', axis=0)
+imputer = imputer.fit(previsores[:, 0:3])
+previsores[:, 0:3] = imputer.transform(previsores[:, 0:3])
+
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+previsores = scaler.fit_transform(previsores)
+
